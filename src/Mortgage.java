73ddd66f6163030530d637460340e6d7a8572600
Emma Jaskowiec <ejaskowiec@pm.me>
2025-03-13 15:09:35 -0500

    Mortgage: add method signatures

--- src/Mortgage.java
+++ src/Mortgage.java
@@ -1,5 +1,48 @@
 public class Mortgage {
-  public static void main(String[] args) {
-    // TODO
-  }
+  /**
+   * Read in from user, validate, and store the amount of the loan
+   */
+  public void storeLoanAmount() {}
+
+  /**
+   * Read in from user, validate, and store the term of the loan
+   */
+  public void storeTerm() {}
+
+  /**
+   * Read in from user, validate, and store the yearly interest rate of the loan
+   */
+  public void storeInterestRate() {}
+
+  /**
+   * Read in from user and store the last name of the customer
+   */
+  public void storeLastName() {}
+  
+  /**
+   * Generate random number, create account number, and store the account number of the customer
+   */
+  public void storeAcctNum() {}
+
+  /**
+   * Calculate and return the monthly payment for the loan
+   *
+   * @return
+   */
+  private double calcMonthlyPayment() {}
+
+  /**
+   * Calculate and return the total payment for the loan
+   *
+   * @return
+   */
+  private double calcTotalPayment() {}
+
+  /**
+   * Display formatted output
+   *
+   * @return
+   */
+  @Override
+  public String toString() {}
 }
