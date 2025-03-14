2eed08b7f00089852e1a52e5a08509599753c5fc
Emma Jaskowiec <ejaskowiec@pm.me>
2025-03-13 19:00:10 -0500

    Mortgage: implement toString(), calcMonthlyPayment(), calcYearlyPayment(); refactor constructors; add promotional constant fields

--- src/Mortgage.java
+++ src/Mortgage.java
@@ -7,40 +7,55 @@ import java.io.IOException;
  * Represents a loan mortgage (i.e. a house loan). TODO: finish description
  */
 public class Mortgage {
-  /** The minimum total loan amount (inclusive). */
+  /** The minimum total principal (inclusive). */
   private static final double LOAN_MIN = 100000.0;
-  /** The maximum total loan amount (exclusive). */
+  /** The maximum total principal (exclusive). */
   private static final double LOAN_MAX = 1000000.0;
   /** The minimum interest rate (inclusive). */
   private static final double INTEREST_MIN = 3.5;
   /** The maximum interest rate (inclusive). */
   private static final double INTEREST_MAX = 9.0;
   /** The minimum mortage term (inclusive). */
-  private static final double TERM_MIN = 15.0;
+  private static final int TERM_MIN = 15;
   /** The maximum mortgage term (inclusive). */
-  private static final double TERM_MAX = 50.0;
+  private static final int TERM_MAX = 50;
 
+  /** The promotional pricipal amount. */
+  private static final double PROMO_LOAN_AMOUNT = 300000;
+  /** The promotional interest rate. */
+  private static final double PROMO_INTEREST = 5.25;
+  /** The promotional mortgage term. */
+  private static final int PROMO_TERM = 30;
+
+  /** The loan amount (principal) in dollars. */
+  private double loanAmount;
   /** The yearly interest rate as a percentage. */
   private double interestRate;
   /** The term of the Mortgage in years. */
-  private double term;
-  /** The total loan amount in dollars. */
-  private double loanAmount;
+  private int term;
   /** The customer's account number. */
-  private int accountNum;
+  private String accountNum;
   /** The customer's last name. */
   private String lastName;
 
   /**
-   * Default constructor
+   * Creates a promotional Mortgage with no account name.
    */
   public Mortgage() {
+    this(PROMO_LOAN_AMOUNT, PROMO_INTEREST, PROMO_TERM, null);
   }
 
   /**
-   * Constructor with customer last name
+   * Creates a promotional Mortgage with the given account name.
    */
-  public Mortgage(double interestRate, double term, double loanAmount,
+  public Mortgage(String lastName) {
+    this(PROMO_LOAN_AMOUNT, PROMO_INTEREST, PROMO_TERM, lastName);
+  }
+
+  /**
+   * Creates a Mortgage with the given fields.
+   */
+  public Mortgage(double loanAmount, double interestRate, double term,
       String lastName) {
   }
 
@@ -85,20 +100,14 @@ public class Mortgage {
   }
 
   /**
-   * Displays formatted output.
-   *
-   * @return A String representing the Mortgage.
-   */
-  @Override
-  public String toString() {
-  }
-
-  /**
    * Calculates the monthly loan payment.
    *
    * @return The monthly loan payment.
    */
   private double calcMonthlyPayment() {
+    double monthlyInterest = this.interestRate / 12.0;
+    double n = Math.pow(1 + monthlyInterest, this.term * 12.0);
+    return this.loanAmount * (monthlyInterest * n / (n - 1));
   }
 
   /**
@@ -107,5 +116,20 @@ public class Mortgage {
    * @return The total loan payment.
    */
   private double calcTotalPayment() {
+    return this.calcMonthlyPayment() * this.term * 12;
+  }
+
+  /**
+   * Displays formatted output.
+   *
+   * @return A String representing the Mortgage.
+   */
+  @Override
+  public String toString() {
+    return String.format(
+        "Account Number: %s\nThe monthly payment is $%,.2f\nThe total payment is $%,.2f\n",
+        this.accountNum,
+        this.calcMonthlyPayment(),
+        this.calcTotalPayment());
   }
 }
