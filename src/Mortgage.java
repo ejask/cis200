dd0eb77aa78fd02cc19b7cb4be33c84c22ebd366
Emma Jaskowiec <ejaskowiec@pm.me>
2025-03-16 20:46:15 -0500

    Mortgage: rewrite constructors; tweak formatting

--- src/Mortgage.java
+++ src/Mortgage.java
@@ -30,22 +30,20 @@ public class Mortgage {
   /** the customer's last name */
   private String lastName;
 
-  /** Creates a promotional Mortgage with no account name. */
+  /** Creates a {@code Mortgage} with field values populated by user input. */
   public Mortgage() {
-    this(PROMO_LOAN_AMOUNT, PROMO_INTEREST, PROMO_TERM, null);
+    this.storeLastName();
+    this.storeAcctNum();
+    this.storeLoanAmount();
+    this.storeInterestRate();
+    this.storeTerm();
   }
 
-  /** Creates a promotional Mortgage with the given account name. */
-  public Mortgage(String lastName) {
-    this(PROMO_LOAN_AMOUNT, PROMO_INTEREST, PROMO_TERM, lastName);
-  }
-
-  /** Creates a Mortgage with the given field values. */
-  public Mortgage(double loanAmount, double interestRate, int term, String lastName) {
+  /** Creates a {@code Mortgage} with the given field values. */
+  public Mortgage(double loanAmount, double interestRate, int term) {
     this.setLoanAmount(loanAmount);
     this.setInterestRate(interestRate);
     this.setTerm(term);
-    this.setLastName(lastName); // also generates account number
   }
 
   /** Reads in from the user, validates, and stores the amount of the loan. */
@@ -61,7 +59,9 @@ public class Mortgage {
       if (!reEnter) {
         System.out.print("Enter the amount of the loan (Ex:120000): ");
       } else {
-        System.out.print("Please re-enter loan amount without $ or commas (Ex:120000): ");
+        System.out.print(
+          "Please re-enter loan amount without $ or commas (Ex:120000): "
+        );
       }
       // remove characters that aren't digits or a decimal points
       input = s.nextLine().trim().replaceAll("[^0-9.]", " ");
@@ -70,7 +70,11 @@ public class Mortgage {
       try {
         amount = Double.parseDouble(input);
       } catch (NumberFormatException e) {
-        System.out.printf("\tValid Loan Amounts are $%.0f - $%.0f (non-inclusive)\n", LOAN_MIN, LOAN_MAX);
+        System.out.printf(
+          "\tValid Loan Amounts are $%.0f - $%.0f (non-inclusive)\n",
+          LOAN_MIN,
+          LOAN_MAX
+        );
         reEnter = true;
         continue; // retry input
       }
@@ -86,7 +90,10 @@ public class Mortgage {
     s.close();
   }
 
-  /** Reads in from the user, validates, and stores the yearly interest rate of the loan. */
+  /**
+   * Reads in from the user, validates, and stores the yearly interest rate of
+   * the loan.
+   */
   public void storeInterestRate() {
     Scanner s = new Scanner(System.in);
     boolean reEnter = false;
@@ -99,7 +106,9 @@ public class Mortgage {
       if (!reEnter) {
         System.out.print("Enter yearly interest rate (Ex: 8.25): ");
       } else {
-        System.out.print("Please re-enter valid yearly interest rate (Ex: 8.25): ");
+        System.out.print(
+          "Please re-enter valid yearly interest rate (Ex: 8.25): "
+        );
       }
       // remove characters that aren't digits or a decimal points
       input = s.nextLine().trim().replaceAll("[^0-9.]", " ");
@@ -108,7 +117,11 @@ public class Mortgage {
       try {
         interest = Double.parseDouble(input);
       } catch (NumberFormatException e) {
-        System.out.printf("\tValid Interest Rates are %.1f%% - %.1f%%\n", INTEREST_MIN, INTEREST_MAX);
+        System.out.printf(
+          "\tValid Interest Rates are %.1f%% - %.1f%%\n",
+          INTEREST_MIN,
+          INTEREST_MAX
+        );
         reEnter = true;
         continue; // retry input
       }
@@ -162,7 +175,11 @@ public class Mortgage {
     s.close();
   }
 
-  /** Creates and stores an account number using the first four letters of the customer's last name and a random {@code int} between 100 (inclusive) to 10000 (exclusive). */
+  /**
+   * Creates and stores an account number using the first four letters of the
+   * customer's last name and a random {@code int} between 100 (inclusive) to
+   * 10000 (exclusive).
+   */
   public void storeAcctNum() {
     this.acctNum = this.lastName.substring(0, 4) + new Random().nextInt(100, 10000);
   }
@@ -201,51 +218,65 @@ public class Mortgage {
   }
 
   /**
-   * @param amount the new loan amount (i.e. principal) of this {@code Mortgage} (in dollars)
-   * @throws IllegalArgumentException if the amount is not between {@value #LOAN_MIN} (inclusive) and {@value #LOAN_MAX} (exclusive)
+   * @param amount the new loan amount (i.e. principal) of this {@code Mortgage}
+   *   (in dollars)
+   * @throws IllegalArgumentException if the amount is not between
+   *   {@value #LOAN_MIN} (inclusive) and {@value #LOAN_MAX} (exclusive)
    */
   public void setLoanAmount(double amount) throws IllegalArgumentException {
     if (amount < LOAN_MIN || amount >= LOAN_MAX) {
-      throw new IllegalArgumentException(
-          String.format("\tValid Loan Amounts are $%.0f - $%.0f (non-inclusive)\n", LOAN_MIN, LOAN_MAX)
-      );
+      throw new IllegalArgumentException(String.format(
+        "\tValid Loan Amounts are $%.0f - $%.0f (non-inclusive)\n",
+        LOAN_MIN,
+        LOAN_MAX
+      ));
     }
     this.loanAmount = amount;
   }
 
   /**
-   * @param term the new interest rate of this {@code Mortgage} (as a percentage)
-   * @throws IllegalArgumentException if the interest rate is not between {@value #INTEREST_MIN} and {@value #INTEREST_MAX} (inclusive)
+   * @param term the new interest rate of this {@code Mortgage} (as a
+   *   percentage)
+   * @throws IllegalArgumentException if the interest rate is not between
+   *   {@value #INTEREST_MIN} and {@value #INTEREST_MAX} (inclusive)
    */
   public void setInterestRate(double interestRate) throws IllegalArgumentException {
     if (interestRate < INTEREST_MIN || interestRate > INTEREST_MAX) {
-      throw new IllegalArgumentException(
-          String.format("\tValid Interest Rates are %.1f%% - %.1f%%\n", INTEREST_MIN, INTEREST_MAX)
-      );
+      throw new IllegalArgumentException(String.format(
+        "\tValid Interest Rates are %.1f%% - %.1f%%\n",
+        INTEREST_MIN,
+        INTEREST_MAX
+      ));
     }
     this.interestRate = interestRate;
   }
 
   /**
    * @param term the new term of this {@code Mortgage} (in years)
-   * @throws IllegalArgumentException if the term is not between {@value #TERM_MIN} and {@value #TERM_MAX} (inclusive)
+   * @throws IllegalArgumentException if the term is not between
+   *   {@value #TERM_MIN} and {@value #TERM_MAX} (inclusive)
    */
   public void setTerm(int term) throws IllegalArgumentException {
     if (term < TERM_MIN || term > TERM_MAX) {
-      throw new IllegalArgumentException(
-          String.format("\tValid Loan Terms are %d-%d\n", TERM_MIN, TERM_MAX)
-      );
+      throw new IllegalArgumentException(String.format(
+        "\tValid Loan Terms are %d-%d\n",
+        TERM_MIN,
+        TERM_MAX
+      ));
     }
     this.term = term;
   }
 
   /**
    * @param name the new last name to associate with the {@code Mortgage}
-   * @throws IllegalArgumentException if the name is not at least one character long
+   * @throws IllegalArgumentException if the name is not at least one character
+   *   long
    */
   public void setLastName(String name) {
     if (name == "") {
-      throw new IllegalArgumentException("Customer last name must be at least one character.");
+      throw new IllegalArgumentException(
+        "Customer last name must be at least one character."
+      );
     }
     name = name.trim();
     name = Character.toUpperCase(name.charAt(0)) + name.substring(1).toLowerCase();
@@ -282,10 +313,10 @@ public class Mortgage {
   @Override
   public String toString() {
     return String.format(
-        "Account Number: %s\nThe monthly payment is $%,.2f\nThe total payment is $%,.2f\n",
-        this.getAcctNum(),
-        this.calcMonthlyPayment(),
-        this.calcTotalPayment()
+      "Account Number: %s\nThe monthly payment is $%,.2f\nThe total payment is $%,.2f\n",
+      this.getAcctNum(),
+      this.calcMonthlyPayment(),
+      this.calcTotalPayment()
     );
   }
 }
