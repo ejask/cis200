752d465b4f52ec69cadcf7e1d10ca3ce1ce28135
Emma Jaskowiec <ejaskowiec@pm.me>
2025-04-14 16:13:20 -0500

    docs(model): add javadoc comments to mortgage class

--- src/Mortgage.java
+++ src/Mortgage.java
@@ -1,5 +1,14 @@
 import java.util.Random;
 
+/**
+ * Mortgage.java
+ * Emma Jaskowiec / Thurs 4:30 (Lab 02B)
+ *
+ * Represents a mortgage (i.e. a house loan) with Big12 Americana Bank. Keeps
+ * track of the principal amount, yearly interest rate, term length, customer
+ * name, and account number (generated from the customer name and four random
+ * digits).
+ */
 public class Mortgage {
   private String accountName;
   private String accountNumber;
@@ -7,44 +16,84 @@ public class Mortgage {
   private double interest;
   private byte term;
 
-  final public double MIN_PRINCIPAL = 100000.0;
-  final public double MAX_PRINCIPAL = 1000000.0;
-  final public double MIN_INTEREST = 3.5;
-  final public double MAX_INTEREST = 9.0;
-  final public byte MIN_TERM = 15;
-  final public byte MAX_TERM = 50;
+  /** The minimum principal in dollars (inclusive). */
+  final public static double MIN_PRINCIPAL = 100000.0;
 
+  /** The maximum principal in dollars (exclusive). */
+  final public static double MAX_PRINCIPAL = 1000000.0;
+
+  /** The minimum yearly interest rate as a percentage (inclusive). */
+  final public static double MIN_INTEREST = 3.5;
+
+  /** The maximum yearly interest rate as a percentage (inclusive). */
+  final public static double MAX_INTEREST = 9.0;
+
+  /** The minimum term length in years (inclusive). */
+  final public static byte MIN_TERM = 15;
+
+  /** The maximum term length in years (inclusive). */
+  final public static byte MAX_TERM = 50;
+
+  /** Constructs a new uninitialized {@link Mortgage} instance. */
   public Mortgage() {}
 
-  public Mortgage(
-    String name,
-    double principal,
-    byte term,
-    double interest
-  ) throws IllegalArgumentException {
+  /**
+   * Constructs a new {@link Mortgage} instance with the given field values.
+   *
+   * @param name  the account name
+   * @param principal  the principal amount in dollars
+   * @param term  the term length in years
+   * @param interest  the yearly interest rate as a percentage
+   */
+  public Mortgage(String name, double principal, byte term, double interest) {
     this.setAccountName(name);
     this.setPrincipal(principal);
     this.setTerm(term);
     this.setInterest(interest);
   }
 
+  /**
+   * Returns the account number of this {@link Mortgage} instance.
+   * 
+   * @return the account number of this {@link Mortgage}
+   */
   public String getAccountNumber() {
     return this.accountNumber;
   }
 
+  /**
+   * Returns the principal amount (in dollars) of this {@link Mortgage} instance.
+   * 
+   * @return the principal of this {@link Mortgage}
+   */
   public double getPrincipal() {
     return this.principal;
   }
 
+  /**
+   * Returns the term length (in years) of this {@link Mortgage} instance.
+   * 
+   * @return the term of this {@link Mortgage}
+   */
   public double getTerm() {
     return this.term;
   }
 
+  /**
+   * Returns the yearly interest rate (as a percentage) of this {@link Mortgage}
+   * instance.
+   * 
+   * @return the yearly interest rate of this {@link Mortgage}
+   */
   public double getInterest() {
     return this.interest;
   }
 
-  // TODO: handle names with spaces?
+  /**
+   * @param name  the account name of this {@link Mortgage}
+   * @throws IllegalArgumentException  if the name is not at least one character
+   *   in length.
+   */
   public void setAccountName(String name) throws IllegalArgumentException {
     name = name.trim();
     if (name.length() == 0) {
@@ -61,6 +110,10 @@ public class Mortgage {
     this.setAccountNumber();
   }
 
+  /**
+   * Generates the account number from the first four letters of the account
+   * name and four randomly generated digits.
+   */
   private void setAccountNumber() {
     final byte NUM_DIGITS = 4;
     StringBuilder sb = new StringBuilder();
@@ -74,6 +127,12 @@ public class Mortgage {
     this.accountNumber = sb.toString();
   }
 
+  /**
+   * @param dollars  the principal amount of this {@link Mortgage}
+   * @throws IllegalArgumentException  if the principal is not between
+   *   ${@value #MIN_PRINCIPAL} (inclusive) and ${@value #MAX_PRINCIPAL}
+   *   (exclusive).
+   */
   public void setPrincipal(double dollars) throws IllegalArgumentException {
     if (dollars < MIN_PRINCIPAL || dollars >= MAX_PRINCIPAL) {
       throw new IllegalArgumentException(String.format(
@@ -85,6 +144,12 @@ public class Mortgage {
     this.principal = dollars;
   }
 
+  /**
+   * @param years  the term length of this {@link Mortgage}
+   * @throws IllegalArgumentException  if the term is not between
+   *   {@value #MIN_TERM} years (inclusive) and {@value #MAX_TERM} years
+   *   (inclusive).
+   */
   public void setTerm(byte years) throws IllegalArgumentException {
     if (years < MIN_TERM || years > MAX_TERM) {
       throw new IllegalArgumentException(String.format(
@@ -96,6 +161,12 @@ public class Mortgage {
     this.term = years;
   }
 
+  /**
+   * @param interest  the yearly interest rate of this {@link Mortgage}
+   * @throws IllegalArgumentException  if the interest is not between
+   *   {@value #MIN_INTEREST}% (inclusive) and {@value #MAX_INTEREST}%
+   *   (inclusive).
+   */
   public void setInterest(double interest) throws IllegalArgumentException {
     if (interest < MIN_INTEREST || interest > MAX_INTEREST) {
       throw new IllegalArgumentException(String.format(
@@ -107,16 +178,35 @@ public class Mortgage {
     this.interest = interest;
   }
 
+  /**
+   * Returns the monthly payment amount (in dollars) of this {@link Mortgage}
+   * instance.
+   *
+   * @return the monthly payment for this {@link Mortgage}
+   */
   private double getMonthlyPayment() {
-    double monthlyInterest = this.interest / 12.0;
+    double monthlyInterest = this.interest / 1200.0;
     double compound = Math.pow(1 + monthlyInterest, this.term * 12.0);
     return this.principal * ((monthlyInterest * compound) / compound - 1);
   }
 
+  /**
+   * Returns the total payment amount (in dollars) of this {@link Mortgage}
+   * instance.
+   *
+   * @return the total payment for this {@link Mortgage}
+   */
   private double getTotalPayment() {
     return this.getMonthlyPayment() * (this.term * 12.0);
   }
 
+  /**
+   * Returns a {@link String} representation of this {@link Mortgage} instance
+   * with the account number, monthly payment, and total payment.
+   * 
+   * @return a {@link String} representing this {@link Mortgage}
+   */
+  @Override
   public String toString() {
     return String.format(
       "Account number: %s\nThe monthly payment is $%.,2f\nThe total payment is $%.,2f",
